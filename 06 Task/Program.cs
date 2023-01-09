1097f29d56caf2feca1b046d75291e511878717e
unknown <yordan.kovachev@gmail.com>
2023-01-09 22:00:16 +0000

    06 Task

---
+++ 06 Task/Program.cs
@@ -0,0 +1,15 @@
+﻿//Декларирайте променлива isMale от тип bool 
+//и присвоете стойност на
+//последната в зависимост от вашия пол
+
+namespace _06_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            Console.WriteLine(isMale);
+        }
+    }
+}
