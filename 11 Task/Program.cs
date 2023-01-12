e3c46f2fd1133ebeb506f5cefd5a4bf0f817f4d2
unknown <yordan.kovachev@gmail.com>
2023-01-12 19:59:15 +0000

    11 Task

---
+++ 11 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿//Write a program
+//that prints on the console isosceles triangle
+//which sides consist of the copyright character "©".
+//https://www.youtube.com/watch?v=rTqBnJ8HrSc  
+
+namespace _11_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
+
+            Console.WriteLine("       \x00A9");
+            Console.WriteLine("      ©©©");
+            Console.WriteLine("     ©©©©©");
+        }
+    }
+}
