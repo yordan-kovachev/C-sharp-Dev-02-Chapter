ffb578f8c29df7476b68a18b0c6df6c775ae8eef
unknown <yordan.kovachev@gmail.com>
2023-01-12 21:12:24 +0000

    13 Task

---
+++ 13 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿//Declare two variables of type int.
+//Assign to them values 5 and 10 respectively.
+//Exchange (swap) their values
+//and print them.
+
+namespace _13_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 10;
+            int tempNumber1 = number2;
+            int tempNumber2 = number1;
+            Console.WriteLine(tempNumber1 + " " + tempNumber2);
+        }
+    }
+}
