3d17f55df8956a09a82d1d15e16bb967a35c4188
unknown <yordan.kovachev@gmail.com>
2023-01-12 18:58:34 +0000

    10 Task

---
+++ 10 Task/Program.cs
@@ -0,0 +1,16 @@
+﻿//Write a program
+//to print a figure in the shape of a heart by the sign "o".
+
+namespace _10_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   oo oo   ");
+            Console.WriteLine("  oооoооo  ");
+            Console.WriteLine("   oоооo   ");
+            Console.WriteLine("     o     ");
+        }
+    }
+}
